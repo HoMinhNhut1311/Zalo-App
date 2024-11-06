9adc3df36964aea938d2a91026926d9048d4abcf
HoMinhNhut1311 <149650549+HoMinhNhut1311@users.noreply.github.com>
2024-11-06 02:13:18 -0800

    Fix gui tin nhan Text ZNS

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -26,24 +26,23 @@ const HomePage = () => {
     }
 
     const accessToken = "4MHpNxOYubKJO75QioFbKHXWT5cxE-8qP7ycVgTjiov2UKjbYq-DBcrYR4YpUVeoUKaVVjvvzYbzU3bJlblG9bSU6q6pMDXBT5en48niftzLLLOkj1MYP54tQ3oMUernQtfBVuzicpHkKWzcl63e3squVbIzUPSkVcj6U9nFqJTYR08zkqpFLNn3AL-B1zvPQ3Oi5wmjt55P2nG_h1A4McSg2poX6irkM1Oe7xihirXC5oXGjLN7Fqz8I5Qn2wzVIp1eKOq4fY9e6LLGrZQoFXGDTJJNBUTCBWilDSqgo49cBomJj3YZG4ONKqcI3yDEPXi5HPyUz4rr314OeG7_P6KP9JJ7Nkn_Cd8tSEzimou8ApTPwn-bEG8aKchd4g4JH4ffJfDgaaLKLaC8fa3KH0PlCJYWIT5qSPDreZ-sF-LG";  
-    const url = "https://openapi.zalo.me/v2.0/oa/message";
+    const url = "https://openapi.zalo.me/v3.0/oa/message/cs";
     const data = {
       "recipient": {
-        "user_id": userId,
+        "user_id": "1630589828152474571"
       },
       "message": {
-        "text": "Hồ Minh Nhựt đã gửi tin nhắn cho bạn", 
-      },
-    };
+        "text": "Hồ Minh Nhựt đã gửi tin nhắn cho bạn"
+      }
+    }
 
     try {
       const response = await axios.post(url, data, {
         headers: {
-          Authorization: `Bearer ${accessToken}`, 
-          "Content-Type": "application/json",
+          "access_token": accessToken,  // Thêm Authorization header
+          "Content-Type": "application/json",  // Đảm bảo header Content-Type là application/json
         },
       });
-      console.log(response);
       
 
       if (response.data.error_code === 0) {
